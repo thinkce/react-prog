2e6064c36e73ce02257306acae51dd4d6bfe3f4b
thinkce <thinkce123@gmail.com>
2022-11-06 09:04:19 +0000

    start up

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+class Home extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <div
+                    className="mydiv"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Accusamus aliquam architecto asperiores autem blanditiis
+                    consequuntur eaque eos esse, exercitationem laboriosam modi odit pariatur
+                    rem sunt suscipit unde vitae voluptatem voluptatibus!
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
